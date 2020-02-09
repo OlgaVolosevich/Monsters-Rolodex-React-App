55866b89959b55af86d8d85393e4f76750896068
Olga Volosevich <olga.lucky.v@gmail.com>
2020-02-09 21:29:40 +0300

    Updates

---
+++ precache-manifest.bb5bfc08133fa57ee6e10f67fde0bce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e182a06c38d3c11716b16229e48de1",
+    "url": "/Monsters-Rolodex-React-App/index.html"
+  },
+  {
+    "revision": "2323983af0a793c625df",
+    "url": "/Monsters-Rolodex-React-App/static/css/main.6944169c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/Monsters-Rolodex-React-App/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monsters-Rolodex-React-App/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2323983af0a793c625df",
+    "url": "/Monsters-Rolodex-React-App/static/js/main.b4bd4938.chunk.js"
+  },
+  {
+    "revision": "e6e8d24b7fd12b4aaee3",
+    "url": "/Monsters-Rolodex-React-App/static/js/runtime-main.e6a83aa0.js"
+  }
+]);
